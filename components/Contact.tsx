27dd2640ac1a2ce9eb60e9ae5670f7f4296f694b
JumaCodes <olumesestevejohn@gmail.com>
2025-08-16 09:31:27 +0100

    Fix the margin-bottom of titles

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -43,7 +43,7 @@ function Contact({}: Props) {
         </div>
         <div className="flex items-center justify-center space-x-5">
           <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
-          <p className="text-xl md:text-2xl">123 Developer Lane</p>
+          <p className="text-xl md:text-2xl">Lagos, Nigeria</p>
         </div>
       </div>
 
