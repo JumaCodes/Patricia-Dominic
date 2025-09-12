3b59a2314719617eeeb9e4329a0f1de4f519990a
JumaCodes <olumesestevejohn@gmail.com>
2025-09-12 17:18:46 +0100

    Miss Dominic Poertfolio done

---
+++ src/app/studio/[[...tool]]/layout.tsx
@@ -0,0 +1,12 @@
+// studio/layout.tsx
+export default function StudioLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
